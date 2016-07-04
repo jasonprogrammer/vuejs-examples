39347b10526a64a4892237a3b621ba32192bcf24
Jason Jones <jason.jones.programmer@gmail.com>
2016-07-04 17:18:45 -0600

    Adding the first VueJS example

--- src/app.js
+++ src/app.js
@@ -1,34 +1,11 @@
 import Vue from 'vue';
-import Component from 'vue-class-component';
-import AppTemplate from './app.tpl';
-import {Menu} from './menu';
 
-@Component({
-    template: AppTemplate,
-    components: {
-        'menu': Menu
+new Vue({
+    el: '#app',
+    data: {
+        'person': {
+            'first': 'Bob',
+            'last': 'Smith'
+        }
     }
-})
-class App {
-    // return initial data
-    data() {
-        return {
-            msg: 123,
-            menu: {
-                items: [{
-                    title: 'Home'
-                }]
-            }
-        };
-    }
-
-    el() {
-        return '#app';
-    }
-
-    ready() {
-    }
-}
-
-// mount
-new App({})
+});
