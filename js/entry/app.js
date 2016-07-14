909e6afdf25dcf6a2ff696abb883f37b06196909
Jason Jones <jason.jones.programmer@gmail.com>
2016-07-14 11:56:26 -0600

    Started working on the 'people' example

--- js/entry/app.js
+++ js/entry/app.js
@@ -1,12 +1,14 @@
-// import Vue from 'vue';
-var Vue = require('../src/vue-2.0.0-beta.1/dist/vue.common');
+var Vue = require('../src/vue-2.0.0-beta.1/dist/vue');
 
 new Vue({
     el: '#app',
     data: {
-        person: {
-            first: 'Bob',
-            last: 'Smith'
-        }
+        people: [{
+            first: 'Joe',
+            last: 'Satriani'
+        },{
+            first: 'Steve',
+            last: 'Vai'
+        }]
     }
 });
