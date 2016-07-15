bf2163998b5f2269456b79e218b1eae2bee0dbeb
Jason Jones <jason.jones.programmer@gmail.com>
2016-07-14 23:47:32 -0600

    Starting to add ES6 class for App component

--- js/entry/app.js
+++ js/entry/app.js
@@ -1,12 +1,31 @@
-// import Vue from 'vue';
-var Vue = require('../src/vue-2.0.0-beta.1/dist/vue.common');
+import Vue from 'vue';
+// var Vue = require('../src/vue-2.0.0-beta.1/dist/vue');
+import Component from 'vue-class-component';
+import AppTemplate from '../src/app.tpl';
+import {Menu} from '../src/menu';
 
-new Vue({
-    el: '#app',
-    data: {
-        person: {
-            first: 'Bob',
-            last: 'Smith'
-        }
+@Component({
+    template: AppTemplate,
+    components: {
+        'menu': Menu
     }
-});
+})
+class App {
+    data() {
+        return {
+            msg: 123,
+            menu: {
+                items: [{
+                    title: 'Home'
+                }]
+            }
+        };
+    }
+
+    el() {
+        return '#app';
+    }
+}
+
+// mount
+new App({})
